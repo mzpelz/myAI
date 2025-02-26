c9250b20e04d008046cb701c6feedc868edc60ea
Pelumi Fadairo <pelumi_fadairo@kenan-flagler.unc.edu>
2025-02-26 01:54:41 +0100

    Update ui.ts
    Changed [PAGE TITLE] to Your KFBS Buddy

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Your KFBS Buddy`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `AI Created by Pelumi Fadairo`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Your KFBS Buddy`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
