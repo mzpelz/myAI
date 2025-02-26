96e45966ee3d7163ffbcad42b830e79377adfcb4
Pelumi Fadairo <pelumi_fadairo@kenan-flagler.unc.edu>
2025-02-26 02:37:15 +0100

    Update identity.ts
    Added that I'm a female

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Pelumi Fadairo`;
-export const OWNER_DESCRIPTION: string = `1Y MBA Student at UNC Kenan-Flagler from Nigeria. Figuring out life as an international student - been there, done that! And tbh, I'm still figuring it out So, I created this AI to help you (us) thrive at KFBS without the guesswork.`;
+export const OWNER_DESCRIPTION: string = `1Y MBA Student at UNC Kenan-Flagler from Nigeria. She's female. Figuring out life as an international student - been there, done that! And tbh, I'm still figuring it out So, I created this AI to help you (us) thrive at KFBS without the guesswork.`;
 
 export const AI_NAME: string = `Your KFBS Buddy`;
 export const AI_TONE: string = `Friendly, upbeat, witty, and supportive—like your go-to classmate who always has the inside scoop.`;
