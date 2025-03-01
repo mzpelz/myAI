1ee94ac344eddbebf770afd239daec8690e0a5ec
Pelumi Fadairo <pelumi_fadairo@kenan-flagler.unc.edu>
2025-03-02 00:10:21 +0100

    Update chat.ts
    Included the full meaning of KFBS to the initial message

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, your guide to navigating life at KFBS. I was created by ${OWNER_NAME} to help international students like you feel at home! What’s on your mind?.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, your guide to navigating life at Kenan-Flagler Business School (KFBS). I was created by ${OWNER_NAME} to help international students like you feel at home! What’s on your mind today?.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Hmm, I don’t have an answer for that right now. Let’s circle back later, or maybe try rephrasing?.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `Whoa, we’re deep in this convo! It's too long. Let's summarize or start fresh if needed.`;
